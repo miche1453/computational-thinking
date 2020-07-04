2184541d330180c28db7dee43342f56f9af28086
miche1453 <67242527+miche1453@users.noreply.github.com>
2020-07-04 00:00:12 -0500

    Update extractframe4.py

--- extractframe4.py
+++ extractframe4.py
@@ -6,11 +6,13 @@ for line in inputfile:
     y = ("Type")
     z = ("Src:")
     a = ("Dst:")
+    #e = ("Internet")
 
     s = line.find(x)
     t = line.find(y)
     b = line.find(z)
     c = line.find(a)
+    #f = line.find(e)
 
     #seedStr = "Type, Src:, Dst:"
     #a_str = "Type, Src:, Dst: "
@@ -38,29 +40,43 @@ for line in inputfile:
     #print(words)
     
 
-    
+
     if (s!= -1):
         #print(line)
         cols = line.split(":")
         print(cols[0])         
-        #print(cols[0][6:])   
+        #print(cols[0][6:])
+        
     if (t!= -1):
         
         #print(line)
-        cols = line.split("(") 
+        cols = line.split(")") and line.split(")")
         print(cols[0])       
-        #print(cols[2][3]) 
-
-    if  (b!= -1) and line.find("Src"):
+        #print(cols[0][2])
         
-        #print(line)
-        cols = line.split("(")
-        #print(cols[0])       
-        print(cols[0][1])
     
-    if (c!= -1):
+    
+    #if (c!= -1):
 
         #print (line)
-        cols = line.split("(")
-        print(cols[0])
-        #print(cols[2][3])
+        #cols = line.split("(")
+        #print(cols[0])
+        #print(cols[0][2])
+
+    if line.find("Src:") != -1 and line.find("(") != -1 and line.find(")") != -1 :
+
+        
+        #print(line)
+        cols = line.split("(") and line.split(")")
+        print(cols[0])       
+        #print(cols[0][2])
+
+    #if line.find("Ethernet II") != -1 and line.find("Internet Protocol Version 4") != -1:
+
+        #cols = line.split("")
+        #print (cols[0][1])
+    #if (f!= -1):
+        #cols = line.split("4")
+        #print(cols[1])  
+        
+
